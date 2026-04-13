7df315c7ca86c13ab74547aeaacbe4b56928df92
Thuo05 <antony30thuo@gmail.com>
2026-04-13 15:37:36 +0300

    Refactor inventory management functions and improve module exports

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,12 +1,28 @@
-// Write your code here
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
+
+function logFirstProduct () {
+  console.log(products [0]);
 
 
+}
+function addProduct(name) {
+  products.push(name);
+}
 
-// Export the necessary parts for testing
-module.exports = {
-  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
-  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
-  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
-  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
+
+function updateProductName(index, newName) {
+  products[index] = newName;
+}
+const removeLastProduct = ()=>{
+
+  products.pop([3])
+}
+
+module.exports ={
+  logFirstProduct,
+  addProduct,
+  updateProductName,
+  removeLastProduct,
   products
-};
+}
+
